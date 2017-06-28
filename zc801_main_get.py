1de01b9ba65836e20b328242ef1efb92a726b1e2
bobo <haiya201405@gmail.com>
2017-06-28 10:34:41 +0800

    .

--- zc801_main_get.py
+++ zc801_main_get.py
@@ -8,7 +8,7 @@ import ztools as zt
 
 import tfb_sys as tfsys
 import tfb_tools as tft
-from zsys import gid_file
+from zsys import gid_file, file_dir
 
 
 def main_get(timeStr='', nday=2):
@@ -17,8 +17,9 @@ def main_get(timeStr='', nday=2):
     # zsys.web_get001txtFg = True
     zsys.web_get001txtFg = False
 
-    rs0 = './'
-    xtfb = tft.fb_init(rs0, gid_file)
+    # rs0 = './'
+    # xtfb = tft.fb_init(rs0, gid_file)
+    xtfb = tft.fb_init(file_dir, gid_file)
     if nday == -1:
         tfsys.xnday_down = xtfb.gid_nday + 10
         print('nday,', tfsys.xnday_down)
